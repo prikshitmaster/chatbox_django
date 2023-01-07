0cdca66ea72487e553c12d750968c1ff16b3a2c8
Prikshit <prikshitcorp@gmail.com>
2023-01-07 16:08:33 +0530

    Initial commit

---
+++ static/script.js
@@ -0,0 +1,10 @@
+
+    (function(d, m){
+        var kommunicateSettings =
+            {"appId":"a0bdf4b7e23f6bbefd00fc24a96c4b64","popupWidget":true,"automaticChatOpenOnNavigation":true};
+        var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+        s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+        var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+        window.kommunicate = m; m._globals = kommunicateSettings;
+    })(document, window.kommunicate || {});
+/* NOTE : Use web server to view HTML files as real-time update will not work if you directly open the HTML file in the browser. */
